c1dd2e9c71c6760b78280c1b51feb9328649f634
Michał Pawełczyk <michal.pawelczyk@gmail.com>
2024-04-14 10:30:02 +0200

    trigger

---
+++ solutions/ownapi.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def get_answer(task_details_response: dict):
+    logger.info("We will solve some 1st World problem !")
+
+    url = "https://hook.eu2.make.com/1m1twarcalg3rdz4ca22uhous7ubrlwn"
+    return url
